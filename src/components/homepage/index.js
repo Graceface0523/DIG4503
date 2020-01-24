525116d90f88bf5835314cdfb66a969af5ec2dce
Graceface0523 <43120745+Graceface0523@users.noreply.github.com>
2020-01-24 15:47:34 -0500

    Add files via upload

---
+++ src/components/homepage/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Homepage extends React.Component{
+    render() {
+        return (
+            <p>{this.props.firstname}</p>
+        );
+    }
+}
+export default Homepage;
